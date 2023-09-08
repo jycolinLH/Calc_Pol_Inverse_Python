9738a7449b293bcb769cd703d9e76de0d6d678dd
JY Colin <jean-yves.colin@univ-lehavre.fr>
2023-09-08 21:59:21 +0200

    Ajout du fichier exemple téléchargé d'Internet

---
+++ calc_v0.py
@@ -0,0 +1,45 @@
+import sys
+
+def get_operands(stack):
+    """ Pop two operands from the stack and return them. """
+    return stack.pop(), stack.pop()
+
+def op_plus(stack):
+    x, y = get_operands(stack)
+    stack.append(x + y)
+
+def op_minus(stack):
+    x, y = get_operands(stack)
+    stack.append(y - x)
+
+def op_multiply(stack):
+    x, y = get_operands(stack)
+    stack.append(x * y)
+
+def op_divide(stack):
+    x, y = get_operands(stack)
+    stack.append(y / x)
+
+def op_power(stack):
+    x, y = get_operands(stack)
+    stack.append(y ** x)
+
+# A dictionary mapping operator tokens to the evaluation functions
+operators = {'+': op_plus,
+             '-': op_minus,
+             '*': op_multiply,
+             '/': op_divide,
+             '**': op_power}
+
+# Initialize an empty stack and get the user's RPN expression, split into tokens
+stack, tokens = [], input('RPN expression:').strip().split()
+for token in tokens:
+    if token in operators:
+        # Operate with the top two numbers on the stack
+        operators[token](stack)
+    else:
+        # We encountered a number: push it onto the top of the stack
+        stack.append(float(token))
+
+# Output the answer
+print(stack[0])
